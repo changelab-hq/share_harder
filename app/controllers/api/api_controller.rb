fd384659aed76114ce3432021e38ac7e5dd4a816
James Rees <jarees@gmail.com>
2018-07-11 19:00:11 +0200

    Update script to allow override of URL

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -2,6 +2,8 @@ class Api::ApiController < ApplicationController
   protect_from_forgery except: [:variant, :record_goal]
 
   def variant
+    lookup_url = params[:experiment_url] || request.referer
+    
     experiment = Experiment.lookup_by_url(request.referer)
     if experiment
       variant = experiment.choose_bandit_variant
