897b4eb2287a4a3ba9a3da649546750683061177
James Rees <jarees@gmail.com>
2018-08-06 13:26:09 +0200

    Get rid of troublesome bang

--- app/helpers/experiments_helper.rb
+++ app/helpers/experiments_helper.rb
@@ -2,6 +2,6 @@ module ExperimentsHelper
 
   # Subset of parameters that should be forwarded to the image
   def image_parameters(params)
-    params.slice!('v', *params.keys.select { |k| k.starts_with? 'm_' })
+    params.slice('v', *params.keys.select { |k| k.starts_with? 'm_' })
   end
 end
