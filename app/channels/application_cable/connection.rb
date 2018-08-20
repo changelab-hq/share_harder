38968f62c8f82cff9234ca2badf7b066e846155c
James Rees <jarees@gmail.com>
2018-08-20 02:03:55 +0200

    Open access to work with live results

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,14 +1,14 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
     identified_by :current_user
- 
+
     def connect
       self.current_user = find_verified_user
     end
- 
+
     private
       def find_verified_user
-        if verified_user = User.find_by(id: cookies.signed['user_id'], admin: true)
+        if verified_user = User.find_by(id: cookies.signed['user_id'], admin: true) || ENV['OPEN_ACCESS'] == 'true'
           verified_user
         else
           reject_unauthorized_connection
