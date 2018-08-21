0f924932f5989038bf7946319f982c85274abb00
Cindy Xie <cindyxie89@gmail.com>
2018-08-21 17:56:10 +0100

    secure cookies false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
+  secure_cookies = false
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
