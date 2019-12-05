9afa91016a2d0d896bcb610249af3e2e00aab10a
jamesr2323 <jarees@gmail.com>
2019-12-05 17:04:39 +0000

    Memoize script

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -2,6 +2,7 @@ class ScriptsController < ApplicationController
   protect_from_forgery except: :script
 
   def script
-    render js: Uglifier.new(harmony: true).compile(render_to_string)
+    $embed_script_js ||= Uglifier.new(harmony: true).compile(render_to_string)
+    render js: $embed_script_js
   end
 end
