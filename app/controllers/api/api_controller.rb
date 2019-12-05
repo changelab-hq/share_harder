878c3a054fc42fe1a5251d2e0f7958adda4d5b4d
jamesr2323 <jarees@gmail.com>
2019-12-05 11:22:54 +0000

    Allow lookup by experiment ID

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -2,9 +2,13 @@ class Api::ApiController < ApplicationController
   protect_from_forgery except: [:variant, :record_goal]
 
   def variant
-    lookup_url = params[:experiment_url].presence || request.referer
+    if params[:experiment_id].present?
+      experiment = Experiment.find(params[:experiment_id])
+    else
+      lookup_url = params[:experiment_url].presence || request.referer
+      experiment = Experiment.lookup_by_url(lookup_url)
+    end
 
-    experiment = Experiment.lookup_by_url(lookup_url)
     if experiment
       variant = experiment.choose_bandit_variant
       data = variant.attributes.slice('id', 'experiment_id', 'description', 'title', 'prefill_text')
--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -2,5 +2,6 @@ class ScriptsController < ApplicationController
   protect_from_forgery except: :script
 
   def script
+    render js: Uglifier.new(harmony: true).compile(render_to_string)
   end
 end
