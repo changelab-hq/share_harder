c52665effeee3e7c726e3871214a59411c22489e
jamesr2323 <jarees@gmail.com>
2019-12-07 19:50:32 +0000

    Allow render to deal with empty template o

--- lib/renderer.rb
+++ lib/renderer.rb
@@ -4,6 +4,6 @@ class Renderer
     params.to_unsafe_hash.each do |k,v| k.starts_with? 'm_'
       merge_params[k[2..-1]] = v
     end
-    Mustache.render(template, merge_params)
+    Mustache.render(template.to_s, merge_params)
   end
 end
