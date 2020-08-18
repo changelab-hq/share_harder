9e6625a82c846d2b319f979073a2cc50ea96957c
James Hulme <james.h@38degrees.org.uk>
2020-08-18 17:28:35 +0100

    fix PersonalisationEditor violations
    - Remove unused imports

--- app/javascript/bundles/Shared/components/PersonalizationEditor.jsx
+++ app/javascript/bundles/Shared/components/PersonalizationEditor.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Mustache from 'mustache'
 
-import Button from '@material-ui/core/Button'
-import Icon from '@material-ui/core/Icon'
-import IconButton from '@material-ui/core/IconButton'
 import TextField from '@material-ui/core/TextField'
 
 const getFieldsFromContent = (content) => {
